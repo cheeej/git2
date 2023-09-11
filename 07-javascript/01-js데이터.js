f346baed61b7ee65227eccd1bddf657be09d3d65
choi11223344 <choi890220@gmail.com>
2023-09-11 11:40:51 +0900

    23-09-11-java

---
+++ 07-javascript/01-js데이터.js
@@ -0,0 +1,14 @@
+//string (문자열)
+
+const string1 = "hello"; //큰 따옴표
+const string2 = "aha"; //작은 따옴표
+const string3 = `yami ${string1} ${1 + 1}`; //백틱은 뒤에 구문 사용/ + 가능
+
+console.log(string1, string2, string3);
+
+//number (숫자)
+// 숫자는 따옴표x, 문자만 사용
+
+const number = 123;
+
+console.log(number);
